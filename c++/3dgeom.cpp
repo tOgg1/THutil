bb8789c1ba5b1c87c1dd0ef8c655da1be7d99910
tOgg1 <tormod.haugland@gmail.com>
2013-10-01 17:31:39 +0200

    Added simple color generation

--- c++/3dgeom.cpp
+++ c++/3dgeom.cpp
@@ -2,61 +2,104 @@
 #include <glm\glm.hpp>
 #include <vector>
 
+
+/*
+Generate colors for all faces of cube
+*/
+void createFaceColorsForNonIndexedCube(	
+	GLfloat* array,
+	glm::vec4 colorFront,
+	glm::vec4 colorBack,
+	glm::vec4 colorRight,
+	glm::vec4 colorLeft,
+	glm::vec4 colorTop,
+	glm::vec4 colorBottom
+)
+{
+	std::vector<glm::vec4> data;
+	data.reserve(36);
+
+	addVec4TripletToVector(&data, colorFront, colorFront, colorFront);
+	addVec4TripletToVector(&data, colorFront, colorFront, colorFront);
+
+	addVec4TripletToVector(&data, colorBack, colorBack, colorBack);
+	addVec4TripletToVector(&data, colorBack, colorBack, colorBack);
+
+	addVec4TripletToVector(&data, colorRight, colorRight, colorRight);
+	addVec4TripletToVector(&data, colorRight, colorRight, colorRight);
+
+	addVec4TripletToVector(&data, colorLeft, colorLeft, colorLeft);
+	addVec4TripletToVector(&data, colorLeft, colorLeft, colorLeft);
+
+	addVec4TripletToVector(&data, colorTop, colorTop, colorTop);
+	addVec4TripletToVector(&data, colorTop, colorTop, colorTop);
+
+	addVec4TripletToVector(&data, colorBottom, colorBottom, colorBottom);
+	addVec4TripletToVector(&data, colorBottom, colorBottom, colorBottom);
+
+	memcpy(array, (GLfloat*)&data[0], sizeof(data[0])*data.size());
+}
+
 /*
 Creates a non indexed cube, returning a cube with 36 points.
 */
 void createNonIndexedCube(GLfloat* array, float sizex, float sizey, float sizez)
 {
-	// Locally this shouldnt impose any problems
-	using namespace glm;
-	vec3 p1, p2, p3, p4, p5, p6, p7, p8;
 
-	p1 = vec3(sizex, sizey, sizez);
-	p2 = vec3(-sizex, sizey, sizez);
-	p3 = vec3(sizex, -sizey, sizez);
-	p4 = vec3(-sizex, -sizey, sizez);
-	p5 = vec3(sizex, sizey, -sizez);
-	p6 = vec3(-sizex, sizey, -sizez);
-	p7 = vec3(sizex, -sizey, -sizez);
-	p8 = vec3(-sizex, -sizey, -sizez);
+	glm::vec3 p1, p2, p3, p4, p5, p6, p7, p8;
 
-	std::vector<vec3> data;
+	p1 = glm::vec3(sizex, sizey, sizez);
+	p2 = glm::vec3(-sizex, sizey, sizez);
+	p3 = glm::vec3(sizex, -sizey, sizez);
+	p4 = glm::vec3(-sizex, -sizey, sizez);
+	p5 = glm::vec3(sizex, sizey, -sizez);
+	p6 = glm::vec3(-sizex, sizey, -sizez);
+	p7 = glm::vec3(sizex, -sizey, -sizez);
+	p8 = glm::vec3(-sizex, -sizey, -sizez);
 
+	std::vector<glm::vec3> data;
+	
 	// Allocate 36 places
 	data.reserve(36);
 
 	//Front face
-	addTriangleToVector(&data, p2, p4, p1);
-	addTriangleToVector(&data, p4, p3, p1);
+	addVec3TripletToVector(&data, p2, p4, p1);
+	addVec3TripletToVector(&data, p4, p3, p1);
 
 	//Back face
-	addTriangleToVector(&data, p5, p7, p6);
-	addTriangleToVector(&data, p7, p8, p6);
+	addVec3TripletToVector(&data, p5, p7, p6);
+	addVec3TripletToVector(&data, p7, p8, p6);
 
 	//Right face
-	addTriangleToVector(&data, p1, p3, p5);
-	addTriangleToVector(&data, p3, p7, p5);
+	addVec3TripletToVector(&data, p1, p3, p5);
+	addVec3TripletToVector(&data, p3, p7, p5);
 
 	//Left face
-	addTriangleToVector(&data, p6, p8, p2);
-	addTriangleToVector(&data, p8, p4, p2);
+	addVec3TripletToVector(&data, p6, p8, p2);
+	addVec3TripletToVector(&data, p8, p4, p2);
 
 	//Top face
-	addTriangleToVector(&data, p6, p2, p5);
-	addTriangleToVector(&data, p2, p1, p5);
+	addVec3TripletToVector(&data, p6, p2, p5);
+	addVec3TripletToVector(&data, p2, p1, p5);
 
 	//Bottom face
-	addTriangleToVector(&data, p3, p7, p4);
-	addTriangleToVector(&data, p7, p8, p4);
+	addVec3TripletToVector(&data, p3, p7, p4);
+	addVec3TripletToVector(&data, p7, p8, p4);
 
 	memcpy(array, (GLfloat*)&data[0], sizeof(data[0])*data.size());
 }
 
-
 /*
 Helper method for adding three points to a std::vector
 */
-void addTriangleToVector(std::vector<glm::vec3>* vec, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3)
+void addVec3TripletToVector(std::vector<glm::vec3>* vec, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3)
+{
+	vec->push_back(p1);
+	vec->push_back(p2);
+	vec->push_back(p3);
+}
+
+void addVec4TripletToVector(std::vector<glm::vec4>* vec, glm::vec4 p1, glm::vec4 p2, glm::vec4 p3)
 {
 	vec->push_back(p1);
 	vec->push_back(p2);
--- c++/3dgeom.h
+++ c++/3dgeom.h
@@ -2,5 +2,18 @@
 #include <glm\glm.hpp>
 #include <vector>
 
+#ifndef CUBE_NOINDEX_SIZE
+#define CUBE_NOINDEX_SIZE 108
+#endif
+
+#ifndef CUBE_NOINDEX_COLOR_SIZE
+#define CUBE_NOINDEX_COLOR_SIZE 144
+#endif
+
+
 void createNonIndexedCube(GLfloat*, float sizex, float sizey, float sizez);
-void addTriangleToVector(std::vector<glm::vec3>* vec, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3);
+void createFaceColorsForNonIndexedCube(GLfloat* array, glm::vec4 colorFront, glm::vec4 colorBack,	glm::vec4 colorRight, glm::vec4 colorLeft, glm::vec4 colorTop, glm::vec4 colorBottom);
+
+//Helper methods
+void addVec3TripletToVector(std::vector<glm::vec3>* vec, glm::vec3 p1, glm::vec3 p2, glm::vec3 p3);
+void addVec4TripletToVector(std::vector<glm::vec4>* vec, glm::vec4 p1, glm::vec4 p2, glm::vec4 p3);
